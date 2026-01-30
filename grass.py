8feedb1f55cb7727ce9fde8d818b778ebd4a70ff
Tiago Neves <yoplayf@gmail.com>
2026-01-30 16:34:25 +0100

    fix grass 3

--- grass.py
+++ grass.py
@@ -24,7 +24,7 @@ class Grass:
         if (self.compteur == 10 or self.compteur == 20) and self.age < 3:
             self.age += 1
 
-        if self.compteur % self.regrowth_time == 0 and rd.randint(0, 5) == 1:
+        if self.compteur % self.regrowth_time == 0:
             voisins = self.world.get_neighbors(self.position)
             if len(voisins) != 0:
                 neighbour_selected = rd.choice(voisins)
