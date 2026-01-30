42eeeb16874ae985fb14eb1b9dbc38c0841502f3
Martin Pouliquen <martin.pouliquen@etu.minesparis.psl.eu>
2026-01-30 15:39:40 +0100

    respawn grass

--- grass.py
+++ grass.py
@@ -16,15 +16,29 @@ class Grass:
         self.world.grasses.remove(self)
         self.dead = 1
 
+    def get_neighbors(self):
+        voisin = []
+        if self.world.is_valid_coordinates((self.position[0]-1, self.position[1])) and not(self.world.is_there_grass((self.position[0]-1, self.position[1]))):
+            voisin.append((self.position[0]-1, self.position[1]))
+        if self.world.is_valid_coordinates((self.position[0]+1, self.position[1])) and not(self.world.is_there_grass((self.position[0]+1, self.position[1]))):
+            voisin.append((self.position[0]+1, self.position[1]))
+        if self.world.is_valid_coordinates((self.position[0], self.position[1]-1)) and not(self.world.is_there_grass((self.position[0], self.position[1]-1))):
+            voisin.append((self.position[0], self.position[1]-1))
+        if self.world.is_valid_coordinates((self.position[0], self.position[1]+1)) and not(self.world.is_there_grass((self.position[0], self.position[1]+1))):
+            voisin.append((self.position[0], self.position[1]+1))
+        return voisin
+
     def update(self):
 
         self.compteur += 1
 
         if (self.compteur == 10 or self.compteur == 20) and self.age < 3:
             self.age += 1
-        if self.dead==1 and compteur>10:
-            x = np.random()
-            if x<0.7:
-                self.age = 0
-                self.dead = 0
-                self.world.grasses.append(self)
+
+        if self.compteur%20 == 0:
+            voisins = self.get_neighbors()
+            if len(voisins)!=0:
+                neighbour_selected = rd.choice(voisins)
+                grass = Grass(self.world, neighbour_selected, 1)
+                self.world.grasses.append(grass)
+
