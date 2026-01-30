660012ad2974131a0f9c6073af9be8867fcb1550
Tiago Neves <yoplayf@gmail.com>
2026-01-30 10:24:44 +0100

    game engine

---
+++ GameEngine.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.locals import *
+import sys
+
+
+class Game:
+    def __init__(self, grid_size, square_size):
+        pygame.init()
+        self.square_size = square_size
+        self.window = pygame.display.set_mode(
+            (grid_size*square_size, grid_size*square_size))
+        self.window.fill((0, 255, 0))
+
+        self.is_running = False
+
+    def set_square(self, pos, color):
+        s = Square(self.square_size, color)
+        self.window.blit(
+            s.surf, (pos[0]*self.square_size, pos[1]*self.square_size))
+
+    def update_window(self):
+        pygame.display.flip()
+
+    def run(self):
+        self.is_running = True
+        while self.is_running:
+            self.game_loop()
+
+            pygame.display.flip()
+
+    def game_loop(self):
+        print("test")
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+class Square(pygame.sprite.Sprite):
+    def __init__(self, size, color):
+        super().__init__()
+        self.surf = pygame.Surface((size, size))
+        self.surf.fill(color)
+
+
+game = Game(40, 15)
+
+game.set_square((1, 1), (255, 0, 0))
+game.run()
